63374c810d03d140975157444793e45b5097e183
Kelvin Ding <weisheng5566@gmail.com>
2023-03-05 02:02:06 +0800

    Added some comments to code for better understanding of the code in the future.

--- HelloWorldToDo/ViewController.swift
+++ HelloWorldToDo/ViewController.swift
@@ -30,15 +30,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textFields = UITextField()
+        //Creation of alerts for user to input values into todo list
         let alert = UIAlertController(title: "Add Items", message: nil, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .destructive))
         alert.addAction(UIAlertAction(title: "Add Items", style: .default, handler: { _ in
+            //Append text to string array
             self.dataItems.append(textFields.text!)
+            //To reload table view to show the new data added
             self.tableView.reloadData()
         }))
-        
+        //Add text field into alerts for user to input values
         alert.addTextField { (textField) in
             textField.placeholder = "Add Items to list"
+            //throwing variables declared in this method out to be used in another method
             textFields = textField
         }
         present(alert, animated: true)
